2573c152304cc9e5cd2405028cc3a147024e03d9
Wamski <waskyjo@gmail.com>
2025-01-31 18:32:59 -0500

    WINDOWgit add Notecards/src/Main.javagit add Notecards/src/Main.java

--- Notecards/src/Main.java
+++ Notecards/src/Main.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        StarterFrame window = new StarterFrame();
+
     }
 }
