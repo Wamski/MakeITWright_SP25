ce3b636f4ff2dc674626e1b50a1081a0c1d99103
Wamski <waskyjo@gmail.com>
2025-01-31 18:37:38 -0500

    frame

---
+++ Notecards/src/StarterFrame.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class StarterFrame extends JFrame {
+    public StarterFrame(){
+        super("Cards");
+        JFrame mainFrame = new JFrame();
+        JPanel content = new JPanel();
+        content.setLayout(new BorderLayout());
+        this.setContentPane(content);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(700,700);
+        this.setLocation(800,50);
+        this.setVisible(true);
+
+    }
+
+}
