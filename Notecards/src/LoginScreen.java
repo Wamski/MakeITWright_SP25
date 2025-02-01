3c00340e8bd0cf680ba64225f33214a2b16f43e5
Evelyn Underwood <evelynunderwood0413@gmail.com>
2025-02-01 13:17:25 -0500

    Robots C:<

--- Notecards/src/LoginScreen.java
+++ Notecards/src/LoginScreen.java
@@ -1,4 +1,3 @@
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -23,14 +22,12 @@ public class LoginScreen extends JFrame implements FocusListener{
 
         //Colors
         Color white = new Color(244, 238, 255);
-
         Color blue5 = new Color(0,59,92);
         Color blue4 = new Color(0,91,115);
         Color blue3 = new Color(0,122,142);
         Color blue2 = new Color(0,153,179);
         Color blue1 = new Color(224,247,250);
         Color yellow = new Color(241,212,161);
-
 
 
         //Main Panel
@@ -40,93 +37,68 @@ public class LoginScreen extends JFrame implements FocusListener{
         content.setBackground(blue3);
         mainPanel.setBorder(BorderFactory.createLineBorder(blue5,10,true));
 
+            //Title Panel
+            JPanel titlePanel = new JPanel();
+            titlePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 80));
+            titlePanel.setBackground(blue3);
 
-        //Title Panel
-        JPanel titlePanel = new JPanel();
-        titlePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 80));
-        titlePanel.setBackground(blue3);
+            JLabel titleLabel = new JLabel("Welcome!");
+            titleLabel.setFont(new Font("Dialog", Font.PLAIN, 60));
 
-        JLabel titleLabel = new JLabel("Welcome!");
-        titleLabel.setFont(new Font("Dialog", Font.PLAIN, 60));
+            titlePanel.add(titleLabel);
+            mainPanel.add(titlePanel);
 
-        titlePanel.add(titleLabel);
-        mainPanel.add(titlePanel);
+            //Username Panel
+            JPanel usernamePanel = new JPanel();
+            usernamePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 110));
+            usernamePanel.setBackground(blue3);
 
-        //Username Panel
-        JPanel usernamePanel = new JPanel();
-        usernamePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 110));
-        usernamePanel.setBackground(blue3);
+            JLabel usernameLabel = new JLabel("Username: ");
+            usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
 
+            usernameTextField = new JTextField("Username");
+            usernameTextField.setFont(new Font("Dialog", Font.PLAIN, 30));
+            usernameTextField.setPreferredSize(new Dimension(153,37));
+            usernameTextField.setForeground(Color.GRAY);
+            usernameTextField.addFocusListener(this);
 
-        JLabel usernameLabel = new JLabel("Username:");
-        usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
+            usernamePanel.add(usernameLabel);
+            usernamePanel.add(usernameTextField);
+            mainPanel.add(usernamePanel);
 
+            //Password Panel
+            JPanel passwordPanel = new JPanel();
+            passwordPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 20));
+            passwordPanel.setBackground(blue3);
 
-        usernameTextField = new JTextField("Username");
-        usernameTextField.setFont(new Font("Dialog", Font.PLAIN, 30));
-        usernameTextField.setPreferredSize(new Dimension(153,37));
-        usernameTextField.setForeground(Color.GRAY);
-        usernameTextField.addFocusListener(this);
+            JLabel passwordLabel = new JLabel("Password: ");
+            passwordLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
 
+            passwordTextField = new JTextField("Password");
+            passwordTextField.setFont(new Font("Dialog", Font.PLAIN, 30));
+            passwordTextField.setPreferredSize(new Dimension(155,35));
+            passwordTextField.setForeground(Color.GRAY);
+            passwordTextField.addFocusListener(this);
 
-        usernamePanel.add(usernameLabel);
-        usernamePanel.add(usernameTextField);
-        mainPanel.add(usernamePanel);
+            passwordPanel.add(passwordLabel);
+            passwordPanel.add(passwordTextField);
+            mainPanel.add(passwordPanel);
 
+            //Incorrect Panel
+            JPanel incorrectPanel = new JPanel();
+            incorrectPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 90));
+            
+            JLabel incorrectLabel = new JLabel("Incorrect Username or Password");
+            incorrectLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
+            incorrectLabel.setForeground(yellow);
+            incorrectLabel.setVisible(false);
+            incorrectPanel.setBackground(blue3);
 
-        //Password Panel
-        JPanel passwordPanel = new JPanel();
-        passwordPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 20));
-        passwordPanel.setBackground(blue3);
-
-
-        JLabel passwordLabel = new JLabel("Password:");
-        passwordLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
-
-
-        passwordTextField = new JTextField("Password");
-        passwordTextField.setFont(new Font("Dialog", Font.PLAIN, 30));
-        passwordTextField.setPreferredSize(new Dimension(155,35));
-        passwordTextField.setForeground(Color.GRAY);
-        passwordTextField.addFocusListener(this);
-
-        passwordPanel.add(passwordLabel);
-        passwordPanel.add(passwordTextField);
-        mainPanel.add(passwordPanel);
-
-        //Incorrect Panel
-        JPanel incorrectPanel = new JPanel();
-        incorrectPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 90));
-
-        JLabel incorrectLabel = new JLabel("Incorrect Username or Password");
-        incorrectLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
-        incorrectLabel.setForeground(yellow);
-        incorrectLabel.setVisible(false);
-        incorrectPanel.setBackground(blue3);
-
-        incorrectPanel.add(incorrectLabel);
-        mainPanel.add(incorrectPanel);
+            incorrectPanel.add(incorrectLabel);
+            mainPanel.add(incorrectPanel);
 
         content.add(mainPanel, BorderLayout.CENTER);
         
-
-        //LoginButton
-        JPanel buttonsPanel = new JPanel();
-        buttonsPanel.setBackground(blue3);
-        buttonsPanel.setLayout(new GridBagLayout());
-
-        buttonsPanel.setBorder(BorderFactory.createLineBorder(blue5,10,true));
-
-        JButton LoginButton = new JButton("Login");
-        LoginButton.addActionListener(e -> {
-            for(CredentialsObj credentialsObj : RegisterScreen.getCredentials()) {
-                if (usernameTextField.getText().equals(credentialsObj.getUsername()) && passwordTextField.getText().equals(credentialsObj.getPassword())){
-                    new StarterFrame();
-                    this.dispose();
-                }
-            }
-            incorrectLabel.setVisible(true);
-        });
 
         //RegisterButton
         JButton RegisterButton = new JButton("Register");
@@ -135,8 +107,46 @@ public class LoginScreen extends JFrame implements FocusListener{
             this.dispose();
         });
 
+
+        //Robot Security
+        JPanel robotPanel = new JPanel();
+        robotPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 0));
+    
+        JCheckBox robotBox = new JCheckBox();
+        robotPanel.add(robotBox);
+
+        JLabel robotLabel = new JLabel("I am not a robot ");
+        robotLabel.setBackground(blue3);
+        robotPanel.add(robotLabel);
+
+
+        //LoginButton
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setBackground(blue3);
+        buttonsPanel.setLayout(new GridBagLayout());
+        buttonsPanel.setBorder(BorderFactory.createLineBorder(blue5,10,true));
+        
+        JButton LoginButton = new JButton("Login");
+        LoginButton.addActionListener(e -> {
+            for(CredentialsObj credentialsObj : RegisterScreen.getCredentials()) {
+                if (usernameTextField.getText().equals(credentialsObj.getUsername()) && passwordTextField.getText().equals(credentialsObj.getPassword()) && robotBox.isSelected()){
+                    new StarterFrame();
+                    this.dispose();
+                }
+            }
+            if(!robotBox.isSelected()){
+                incorrectLabel.setText("Please select I am not a robot");
+                incorrectLabel.setVisible(true);
+            }else{
+                incorrectLabel.setText("Incorrect Username or Password");
+                incorrectLabel.setVisible(true);
+            }
+        });
+
         buttonsPanel.add(RegisterButton);
         buttonsPanel.add(LoginButton);
+        buttonsPanel.add(robotPanel);
+
         content.add(buttonsPanel, BorderLayout.SOUTH);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- Notecards/src/RegisterScreen.java
+++ Notecards/src/RegisterScreen.java
@@ -36,132 +36,122 @@ public class RegisterScreen extends JFrame implements FocusListener {
         //Main Panel
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new GridLayout(5,1));
-
         mainPanel.setBackground(blue3);
         content.setBackground(blue3);
         mainPanel.setBorder(BorderFactory.createLineBorder(blue5,10,true));
 
-        //Title Panel
-        JPanel titlePanel = new JPanel();
-        titlePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 45));
-        titlePanel.setBackground(blue3);
+            //Title Panel
+            JPanel titlePanel = new JPanel();
+            titlePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 45));
+            titlePanel.setBackground(blue3);
 
+            JLabel titleLabel = new JLabel("Sign Up!");
+            titleLabel.setFont(new Font("Dialog", Font.PLAIN, 60));
+            
+            titlePanel.add(titleLabel);
+            mainPanel.add(titlePanel);
 
-        JLabel titleLabel = new JLabel("Sign Up!");
-        titleLabel.setFont(new Font("Dialog", Font.PLAIN, 60));
+            //Username Panel
+            JPanel usernamePanel = new JPanel();
+            usernamePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 70));
+            usernamePanel.setBackground(blue3);
 
+            JLabel usernameLabel = new JLabel("Username: ");
+            usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
 
-        //Username Panel
-        JPanel usernamePanel = new JPanel();
-        usernamePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 70));
-        usernamePanel.setBackground(blue3);
+            usernameTextField = new JTextField("Username");
+            usernameTextField.setFont(new Font("Dialog", Font.PLAIN, 30));
+            usernameTextField.setPreferredSize(new Dimension(200,38));
+            usernameTextField.setForeground(Color.GRAY);
+            usernameTextField.addFocusListener(this);
 
-        JLabel usernameLabel = new JLabel("Username: ");
-        usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
+            usernamePanel.add(usernameLabel);
+            usernamePanel.add(usernameTextField);
+            mainPanel.add(usernamePanel);
 
-        usernameTextField = new JTextField("Username");
-        usernameTextField.setFont(new Font("Dialog", Font.PLAIN, 30));
-        usernameTextField.setPreferredSize(new Dimension(200,38));
-        usernameTextField.setForeground(Color.GRAY);
-        usernameTextField.addFocusListener(this);
+            //Password Panel
+            JPanel passwordPanel = new JPanel();
+            passwordPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 55));
+            passwordPanel.setBackground(blue3);
 
-        usernamePanel.add(usernameLabel);
-        usernamePanel.add(usernameTextField);
-        mainPanel.add(usernamePanel);
+            JLabel passwordLabel = new JLabel("Password: ");
+            passwordLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
 
-        //Password Panel
-        JPanel passwordPanel = new JPanel();
-        passwordPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 55));
-        passwordPanel.setBackground(blue3);
+            passwordTextField = new JTextField("Password");
+            passwordTextField.setFont(new Font("Dialog", Font.PLAIN, 30));
+            passwordTextField.setPreferredSize(new Dimension(200,35));
+            passwordTextField.setForeground(Color.GRAY);
+            passwordTextField.addFocusListener(this);
 
-        JLabel passwordLabel = new JLabel("Password: ");
-        passwordLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
+            passwordPanel.add(passwordLabel);
+            passwordPanel.add(passwordTextField);
+            mainPanel.add(passwordPanel);
 
-        passwordTextField = new JTextField("Password");
-        passwordTextField.setFont(new Font("Dialog", Font.PLAIN, 30));
-        passwordTextField.setPreferredSize(new Dimension(200,35));
-        passwordTextField.setForeground(Color.GRAY);
-        passwordTextField.addFocusListener(this);
+            //Confirmation Panel
+            JPanel confirmationPanel = new JPanel();
+            confirmationPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 45));
+            confirmationPanel.setBackground(blue3);
 
-        passwordPanel.add(passwordLabel);
-        passwordPanel.add(passwordTextField);
-        mainPanel.add(passwordPanel);
+            JLabel confirmationLabel = new JLabel("Confirmation: ");
+            confirmationLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
 
-        //Confirmation Panel
-        JPanel confirmationPanel = new JPanel();
-        confirmationPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 45));
-        confirmationPanel.setBackground(blue3);
+            confirmationTextField = new JTextField("Confirmation");
+            confirmationTextField.setFont(new Font("Dialog", Font.PLAIN, 30));
+            confirmationTextField.setPreferredSize(new Dimension(200,35));
+            confirmationTextField.setForeground(Color.GRAY);
+            confirmationTextField.addFocusListener(this);
 
-        JLabel confirmationLabel = new JLabel("Confirmation: ");
-        confirmationLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
+            confirmationPanel.add(confirmationLabel);
+            confirmationPanel.add(confirmationTextField);
 
-        confirmationTextField = new JTextField("Confirmation");
-        confirmationTextField.setFont(new Font("Dialog", Font.PLAIN, 30));
-        confirmationTextField.setPreferredSize(new Dimension(200,35));
-        confirmationTextField.setForeground(Color.GRAY);
-        confirmationTextField.addFocusListener(this);
+            mainPanel.add(confirmationPanel);
 
-        confirmationPanel.add(confirmationLabel);
-        confirmationPanel.add(confirmationTextField);
+            //Incorrect Panel
+            JPanel incorrectPanel = new JPanel();
+            incorrectPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 80));
+            incorrectPanel.setBackground(blue3);
 
-        mainPanel.add(confirmationPanel);
-
-        //Incorrect Panel
-        JPanel incorrectPanel = new JPanel();
-        incorrectPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 80));
-        incorrectPanel.setBackground(blue3);
-
-        JLabel incorrectLabel = new JLabel("Please enter a username and password");
-        incorrectLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
-        incorrectLabel.setForeground(yellow);
-        incorrectLabel.setVisible(false);
-
-        incorrectPanel.add(incorrectLabel);
-        mainPanel.add(incorrectPanel);
+            JLabel incorrectLabel = new JLabel("Please enter a username and password");
+            incorrectLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
+            incorrectLabel.setForeground(yellow);
+            incorrectLabel.setVisible(false);
+            
+            incorrectPanel.add(incorrectLabel);
+            mainPanel.add(incorrectPanel);
 
         content.add(mainPanel, BorderLayout.CENTER);
-
+        
         //ButtonsPanel
         JPanel buttonsPanel = new JPanel();
         buttonsPanel.setBackground(blue3);
         buttonsPanel.setLayout(new GridBagLayout());
-
         buttonsPanel.setBorder(BorderFactory.createLineBorder(blue5,10,true));
 
         
-        //RegisterButton
-        JButton LoginButton = new JButton("Register");
-        LoginButton.addActionListener(e -> {
-            boolean taken = false;
-            //if username is blank
-            if(usernameTextField.getText().equals("")){
-                incorrectLabel.setText("Please enter a username");
-                incorrectLabel.setVisible(true);
-            //if password is blank
-            }else if(passwordTextField.getText().equals("")){
-                incorrectLabel.setText("Please enter a password");
-                incorrectLabel.setVisible(true);
-
+            //RegisterButton
+            JButton LoginButton = new JButton("Register");
+            LoginButton.addActionListener(e -> {
+                boolean taken = false;
+                //if username is blank
+                if(usernameTextField.getText().equals("")){
+                    incorrectLabel.setText("Please enter a username");
+                    incorrectLabel.setVisible(true);
+                //if password is blank
+                }else if(passwordTextField.getText().equals("")){
+                    incorrectLabel.setText("Please enter a password");
+                    incorrectLabel.setVisible(true);
                 //if confirmation is blank
-            }else if(confirmationTextField.getText().equals("")){
-                incorrectLabel.setText("Please enter a confirmation");
-                incorrectLabel.setVisible(true);
+                }else if(confirmationTextField.getText().equals("")){
+                    incorrectLabel.setText("Please enter a confirmation");
+                    incorrectLabel.setVisible(true);
                 //if confirmation does not match password
-            }else if(!(confirmationTextField.getText().equals(passwordTextField.getText()))){
-                incorrectLabel.setText("Passwords must match");
-                incorrectLabel.setVisible(true);
-
+                }else if(!(confirmationTextField.getText().equals(passwordTextField.getText()))){
+                    incorrectLabel.setText("Passwords must match");
+                    incorrectLabel.setVisible(true);
                 //if password is less than 8 characters
-            }else if(passwordTextField.getText().length() < 8){
-                incorrectLabel.setText("Password must be at least 8 characters long");
-                incorrectLabel.setVisible(true);
-            }else{
-                //if username matches a taken username
-                for(CredentialsObj credentialsObj : credentials) {
-                    if (usernameTextField.getText().equals(credentialsObj.getUsername())){
-                        incorrectLabel.setText("Username already taken");
+                }else if(passwordTextField.getText().length() < 8){
+                        incorrectLabel.setText("Password must be at least 8 characters long");
                         incorrectLabel.setVisible(true);
-
                 }else{
                     //if username matches a taken username
                     for(CredentialsObj credentialsObj : credentials) {
@@ -175,34 +165,19 @@ public class RegisterScreen extends JFrame implements FocusListener {
                     if(taken == false){
                         CredentialsObj login = new CredentialsObj(usernameTextField.getText(), passwordTextField.getText());
                         credentials.add(login);
-
-                        //credentials.addCredentials(login);
-
                         incorrectLabel.setVisible(false);
                         new LoginScreen();
                         this.dispose();
-
                     }
                 }
-                //otherwise make credential
-                if(taken == false){
-                    CredentialsObj login = new CredentialsObj(usernameTextField.getText(), passwordTextField.getText());
-                    credentials.add(login);
-                    incorrectLabel.setVisible(false);
-                    //new LoginScreen();
-                    // this.dispose();
-                }
-            }
-        });
+            });
 
-
-        //BackButton
-        JButton BackButton = new JButton("Back");
-        BackButton.addActionListener(e -> {
-            new LoginScreen();
-            this.dispose();
-        });
-
+            //BackButton
+            JButton BackButton = new JButton("Back");
+            BackButton.addActionListener(e -> {
+                new LoginScreen();
+                this.dispose();
+            });
 
         buttonsPanel.add(BackButton);
         buttonsPanel.add(LoginButton);
@@ -221,12 +196,11 @@ public class RegisterScreen extends JFrame implements FocusListener {
 
     @Override
     public void focusGained(FocusEvent e) {
-        if(e.getSource() == passwordTextField) {
-            if(passwordTextField.getText().equals("Password")) {
+        if (e.getSource() == passwordTextField) {
+            if (passwordTextField.getText().equals("Password")) {
                 passwordTextField.setText(""); // Clear placeholder text
                 passwordTextField.setForeground(Color.BLACK); // Change text color to black
             }
-
         } else if (e.getSource() == usernameTextField) {
             if (usernameTextField.getText().equals("Username")) {
                 usernameTextField.setText(""); // Clear placeholder text
